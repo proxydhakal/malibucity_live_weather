dd39b22c8a4183afc8786c798d6f0bed539fd700
Shekhar Dhakal <33958757+proxydhakal@users.noreply.github.com>
2019-09-28 16:11:22 +0545

    Update settings.py

--- live_streaming_site/settings.py
+++ live_streaming_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's@5s==$0(&x2(3a=b$u501*$%6f$-3v9o$nq3$%a8b_-in8xq8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['142.93.185.54']
 
 
 # Application definition
@@ -129,3 +129,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
